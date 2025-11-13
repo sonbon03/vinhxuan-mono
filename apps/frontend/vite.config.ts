2bf00e93bcc2d453cef4753808c723ba4de63ba9
sonbon03 <tson20022003@gmail.com>
2025-11-13 13:11:10 +0700

    chore: update vercal frontend

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths({
+      root: '../../',
+      projects: ['./apps/frontend/tsconfig.json'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
